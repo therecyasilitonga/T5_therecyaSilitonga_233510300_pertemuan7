34e3a262109a81bd75b3d463b69952298114422b
therecyasilitonga <therecyasilitonga@student.uir.ac.id>
2025-06-23 06:34:31 +0700

    to do input

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,34 @@
 <template>
-  <div>
-    <button @click="counter.increment()">Tambah (+)</button>
-    <button @click="counter.decrement()">Kurang (-)</button>
+  <div class="form">
+    <input v-model="title" placeholder="Tambah todo baru..." @keyup.enter="submit" />
+    <button @click="submit">Tambah</button>
   </div>
 </template>
 
 <script setup>
-import { useCounterStore } from '../stores/todoStore'
+import { ref } from 'vue';
+import { useTodoStore } from '../stores/todoStore';
 
-const counter = useCounterStore()
+const store = useTodoStore();
+const title = ref('');
+
+const submit = () => {
+  store.addTodo(title.value);
+  title.value = '';
+};
 </script>
+
+<style scoped>
+.form {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+input {
+  flex: 1;
+  padding: 6px;
+}
+button {
+  padding: 6px 12px;
+}
+</style>
