fe1fd46a67a3b9f9c4e1290f30ac1741e54b86a9
therecyasilitonga <therecyasilitonga@student.uir.ac.id>
2025-06-23 06:35:32 +0700

    to do list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
-    <h3>{{ counter.name }}</h3>
-    <p>Nilai saat ini: {{ counter.count }}</p>
-    <p>Nilai ganda: {{ counter.doubleCount }}</p>
-  </div>
+  <ul>
+    <li v-for="todo in store.todos" :key="todo.id" class="todo-item">
+      <input type="checkbox" v-model="todo.completed" @change="store.toggleComplete(todo.id)" />
+      <span :class="{ done: todo.completed }">{{ todo.title }}</span>
+      <button @click="store.deleteTodo(todo.id)">Hapus</button>
+    </li>
+    <p><strong>{{ store.incompleteCount }}</strong> tugas belum selesai</p>
+  </ul>
 </template>
 
 <script setup>
-import { useCounterStore } from '../stores/todoStore'
-
-// Gunakan store di dalam komponen
-const counter = useCounterStore()
+import { useTodoStore } from '../stores/todoStore';
+const store = useTodoStore();
 </script>
+
+<style scoped>
+.todo-item {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 10px;
+}
+.done {
+  text-decoration: line-through;
+  color: gray;
+}
+</style>
